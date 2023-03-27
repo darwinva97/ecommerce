f98c80fcc50f544012387a2606c88934fa68470d
darwinva97 <darwin97.va@hotmail.com>
2023-03-26 21:26:00 -0500

    avance2

--- src/components/BtnPaypal/index.tsx
+++ src/components/BtnPaypal/index.tsx
@@ -1,22 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import React from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { env } from "@/env.mjs";
+import { useStore } from "@/store";
+import { api } from "@/utils/api";
 
 export const BtnPaypal = () => {
+  const { cart } = useStore();
+  const createMutation = api.paypal.create.useMutation();
   return (
     <PayPalScriptProvider
       options={{ "client-id": env.NEXT_PUBLIC_PAYPAL_CLIENT_ID }}
     >
       <PayPalButtons
+        createOrder={async () => {
+          try {
+            const result = await createMutation.mutateAsync({
+              products: cart.map((p) => ({
+                quantity: p.quantity,
+                product: p.variant,
+              })),
+            });
+            console.log(result);
+            const id = (JSON.parse(result.payload) as any)?.responsePaypal
+              ?.result?.id;
+            console.log(id);
+            return id;
+          } catch (error) {
+            console.log(error);
+            return String(error);
+          }
+        }}
         onCancel={(data) => console.log("compra cancelada", data)}
         onApprove={async (data, actions) => {
           console.log(data);
           void (await actions?.order?.capture());
         }}
         style={{ layout: "horizontal", color: "blue" }}
-      >
-        Hola
-      </PayPalButtons>
+      />
     </PayPalScriptProvider>
   );
 };
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,56 +1,59 @@
 import { ClientLayout } from "@/components/layouts/Client";
 import type { CustomPageComponent } from "@/pages/_app";
-import { Button, Center, Container, Table, Title } from "@mantine/core";
+import { Center, Container, Table, Title } from "@mantine/core";
 import { useStore } from "@/store";
 import { BtnPaypal } from "@/components/BtnPaypal";
+import { useEffect, useState } from "react";
 
 const CartPage: CustomPageComponent = () => {
   const { cart } = useStore();
+  const [total, setTotal] = useState<number | null>(null);
 
-  const total = cart.reduce(
-    (total, { variant: { offer_price }, quantity }) =>
-      (total += offer_price * quantity * quantity),
-    0
-  );
+  useEffect(() => {
+    const total = cart.reduce(
+      (total, { variant: { offer_price }, quantity }) =>
+        (total += offer_price * quantity * quantity),
+      0
+    );
+    setTotal(total);
+  }, []);
 
   return (
     <Container>
       <Title>Cart</Title>
       <Table my="lg">
-          <thead>
-            <tr>
-              <th>N°</th>
-              <th>Nombre</th>
-              <th>Precio</th>
-              <th>Oferta</th>
-              <th>Monto</th>
-              <th>Cantidad</th>
+        <thead>
+          <tr>
+            <th>N°</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Oferta</th>
+            <th>Monto</th>
+            <th>Cantidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map(({ variant, quantity }, index) => (
+            <tr key={variant.id}>
+              <td>{index + 1}</td>
+              <td>{variant.name}</td>
+              <td>{variant.price}</td>
+              <td>{variant.offer_message}</td>
+              <td>{quantity}</td>
+              <td>{(variant.offer_price * quantity).toFixed(2)}</td>
             </tr>
-          </thead>
-          <tbody>
-            {cart.map(({ variant, quantity }, index) => (
-              <tr key={variant.id}>
-                <td>{index + 1}</td>
-                <td>{variant.name}</td>
-                <td>{variant.price}</td>
-                <td>{variant.offer_message}</td>
-                <td>{quantity}</td>
-                <td>{(variant.offer_price * quantity).toFixed(2)}</td>
-              </tr>
-            ))}
-            <tr>
-              <td colSpan={5}>Subtotal:</td>
-              <td>{total}</td>
-            </tr>
-          </tbody>
-        </Table>
+          ))}
+          <tr>
+            <td colSpan={5}>Subtotal:</td>
+            <td>{total}</td>
+          </tr>
+        </tbody>
+      </Table>
 
       {/* <Button color="dark" w="100%" onClick={initPayment}>
         Iniciar pago
       </Button> */}
-      <Center>
-        <BtnPaypal />
-      </Center>
+      <Center>{total && <BtnPaypal />}</Center>
     </Container>
   );
 };
